880601debb290517049d6dd2ef998cfe0794a0ea
Walter Bernal <wltrbm@gmail.com>
2023-09-23 12:09:06 -0700

    Initial Commit

---
+++ Gestures-In-Depth/Gestures_In_DepthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestures_In_DepthApp.swift
+//  Gestures-In-Depth
+//
+//  Created by Walter Bernal Montero on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Gestures_In_DepthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
